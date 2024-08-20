d89a7c2ecc3d242fd625a881d967706fec217aa9
kim <ksh251024@gmail.com>
2024-08-20 09:57:59 +0900

    파일 > 새로 만들기 구현, 도움말 > 도움말 보기 구현

--- WinFormAdv/Form1.Designer.cs
+++ WinFormAdv/Form1.Designer.cs
@@ -75,6 +75,7 @@
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -114,8 +115,9 @@
             // 새로만들기ToolStripMenuItem
             // 
             this.새로만들기ToolStripMenuItem.Name = "새로만들기ToolStripMenuItem";
-            this.새로만들기ToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.새로만들기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.새로만들기ToolStripMenuItem.Text = "새로 만들기";
+            this.새로만들기ToolStripMenuItem.Click += new System.EventHandler(this.새로만들기ToolStripMenuItem_Click);
             // 
             // 새창ToolStripMenuItem
             // 
@@ -161,7 +163,7 @@
             // 끝내ToolStripMenuItem
             // 
             this.끝내ToolStripMenuItem.Name = "끝내ToolStripMenuItem";
-            this.끝내ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.끝내ToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
             this.끝내ToolStripMenuItem.Text = "끝내기";
             this.끝내ToolStripMenuItem.Click += new System.EventHandler(this.끝내ToolStripMenuItem_Click);
             // 
@@ -358,8 +360,9 @@
             // 도움말보기ToolStripMenuItem
             // 
             this.도움말보기ToolStripMenuItem.Name = "도움말보기ToolStripMenuItem";
-            this.도움말보기ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.도움말보기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.도움말보기ToolStripMenuItem.Text = "도움말 보기";
+            this.도움말보기ToolStripMenuItem.Click += new System.EventHandler(this.도움말보기ToolStripMenuItem_Click);
             // 
             // 피드백보내ToolStripMenuItem
             // 
@@ -413,11 +416,23 @@
             this.toolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
             this.toolStripProgressBar1.Value = 50;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(738, 361);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "여기에 작성하세요.";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -481,6 +496,7 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
